c1d37fffb40dff4ec54436e242e21bdc4588daa4
Robert Pirtle <astropirtle@gmail.com>
2019-05-26 12:30:33 -0700

    trianglify polygons on load

--- polyLoader.js
+++ polyLoader.js
@@ -16,13 +16,29 @@ function readPolys(content) {
   }
 
   const numFaces = parseInt(lines[numPoints + 1]);
-  const faces = []
+  let faces = []
   for (let i = 0; i < numFaces; i++) {
-    let face = lines[numPoints + i + 2];
-    faces.push(face.split(',').map(p => parseInt(p)));
+    let face = lines[numPoints + i + 2].split(',').map(p => parseInt(p));
+    if(face.length < 3)
+        console.warn(`WARNING! face ${i} has insufficent vertices in current polygon`);
+    else if(face.length === 3)
+      faces.push(face);
+    else
+      faces = faces.concat(trianglify(face));
   }
 
   return `module.exports = () => (${JSON.stringify({vertices, faces})})`;
+}
+
+// take a poly of >3 sides and convert it into subtriangles
+function trianglify(face) {
+  const numTris = face.length - 2;
+  const triangles = [];
+  for (let i = 1; i <= numTris; i++) {
+    const tri = [face[0], face[i], face[i+1]];
+    triangles.push(tri);
+  }
+  return triangles;
 }
 
 module.exports = readPolys;
